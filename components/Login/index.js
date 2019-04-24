460c08f641ca966f920ee5edd405fd62be180e50
dasbego <gbego91@gmail.com>
2019-04-23 23:48:31 -0600

    Switch to NextJs instead of CRA

---
+++ components/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Login extends React.Component {
+  componentDidMount() {
+
+  }
+  render() {
+    return (<div>
+      My login
+    </div>);
+  }
+}
+
+export default Login;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../components/Login';
+
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+export default LoginPage;
