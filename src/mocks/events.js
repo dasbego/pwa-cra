2148da79d61ed7b2e06a15ca38e4fdffbe84c2ce
dasbego <gbego91@gmail.com>
2019-04-26 22:26:49 -0600

    Add mocks for rendering events

---
+++ src/mocks/events.js
@@ -0,0 +1,55 @@
+export default [{
+  id: '1',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: false,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+},{
+  id: '2',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: false,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+},{
+  id: '3',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: false,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+},{
+  id: '4',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: false,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+},{
+  id: '5',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: true,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+},{
+  id: '6',
+  name: 'Sneaker Feaver',
+  description: '',
+  date: '29 de Julio',
+  expired: true,
+  price: 500,
+  location: 'Ciudad de México',
+  image: 'https://via.placeholder.com/100'
+}]
